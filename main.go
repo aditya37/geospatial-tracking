1a4284985ea08799db85d773b5b20a97b20b07f7
Aditya Kurnia Rahman <aditya.krohman@gmail.com>
2023-03-18 18:28:12 +0700

    remove debug env

--- main.go
+++ main.go
@@ -2,20 +2,15 @@ package main
 
 import (
 	"log"
-	"os"
 
 	"github.com/aditya37/geospatial-tracking/service"
-	"github.com/joho/godotenv"
 )
 
-func init() {
-	godotenv.Load()
-}
+// func init() {
+// 	godotenv.Load()
+// }
 
 func main() {
-	log.Println(os.Getenv("ELASTIC_APM_SERVICE_NAME"))
-	log.Println(os.Getenv("ELASTIC_APM_SERVER_URL"))
-
 	svc, err := service.NewGrpc()
 	if err != nil {
 		log.Fatal(err)
